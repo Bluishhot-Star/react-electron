4f6784543ab8b354eaa614b479de614c1e5b1dba
남경 <lnamkyung@gmail.com>
2024-08-12 11:56:51 +0900

    검사페이지 스크롤 버그 해결

--- src/pages/measurementPage.js
+++ src/pages/measurementPage.js
@@ -2337,7 +2337,7 @@ useEffect(()=>{
           </div>
 
           <div className="history-container">
-              <div className="slider">
+              <div className="slider measure">
                 {
                 totalData == " " || totalData == "Empty resource" || !totalData ? null :
                   totalData.trials.map((item, index)=>(
